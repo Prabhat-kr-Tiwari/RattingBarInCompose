02d6d64b87b53e37dc6c8c94ad017893b16c9766
Prabhat kumar tiwari <sonukbc6@gmail.com>
2024-05-31 11:10:36 +0530

    first commit

---
+++ app/src/main/java/com/prabhat/starrattingbar/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.prabhat.starrattingbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.prabhat.starrattingbar.ui.theme.StarRattingBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StarRattingBarTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        var rating_1 by remember{
+                            mutableDoubleStateOf(3.5)
+                        }
+                        RattingBar(
+                            modifier = Modifier.size(50.dp),
+                            rating=rating_1,
+                            starsColor = Color.Yellow
+                        ) {
+                            rating_1=it
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StarRattingBarTheme {
+        Greeting("Android")
+    }
+}
